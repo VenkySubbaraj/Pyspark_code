c1a8de92e51007883c8e582de24408439bcf4d86
VenkatachalapathySubbaraj <87265462+VenkySubbaraj@users.noreply.github.com>
2023-11-22 13:00:13 +0530

    Update collecting _data_from_redshift_to_s3.py

--- collecting _data_from_redshift_to_s3.py
+++ collecting _data_from_redshift_to_s3.py
@@ -1,44 +1,49 @@
-# Import required libraries
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import *
-from pyspark.sql.types import *
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
 
-# Configure Redshift connection properties
-hostname = "<redshift-hostname>"
-port = "<redshift-port>"
-database = "<redshift-database>"
-user = "<redshift-user>"
-password = "<redshift-password>"
-table = "<redshift-table>"
+# Read command line arguments
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
 
-# Configure S3 output path
-s3_output_path = "s3://<s3-bucket>/<s3-path>"
+# Set up the GlueContext and SparkContext
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
 
-# Create SparkSession
-spark = SparkSession.builder.appName("Extract_Redshift_Data").getOrCreate()
+# Define Redshift connection options
+redshift_options = {
+    "url": "jdbc:redshift://your-redshift-cluster-url:5439/your-database",
+    "dbtable": "your_table",
+    "user": "your-username",
+    "password": "your-password",
+    "redshiftTmpDir": "s3://your-s3-temp-directory/"
+}
 
-# Load data from Redshift into PySpark DataFrame
-df = spark.read \
-  .format("com.databricks.spark.redshift") \
-  .option("url", f"jdbc:redshift://{hostname}:{port}/{database}") \
-  .option("user", user) \
-  .option("password", password) \
-  .option("dbtable", table) \
-  .load()
+# Define S3 output options
+s3_output_path = "s3://your-s3-bucket/your-output-directory/"
 
-# Save the PySpark DataFrame to S3 in Parquet format
-df.write \
-  .format("parquet") \
-  .option("compression", "snappy") \
-  .option("path", s3_output_path) \
-  .option("spark.hadoop.fs.s3a.endpoint", "s3.amazonaws.com") \
-  .option("spark.hadoop.fs.s3a.access.key", "<aws-access-key>") \
-  .option("spark.hadoop.fs.s3a.secret.key", "<aws-secret-key>") \
-  .option("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
-  .option("spark.hadoop.fs.s3a.fast.upload", "true") \
-  .option("spark.sql.parquet.compression.codec", "snappy") \
-  .mode("overwrite") \
-  .save()
+# Read data from Redshift
+datasource = glueContext.create_dynamic_frame.from_catalog(
+    database = "your_glue_catalog_database",
+    table_name = "your_glue_catalog_table",
+    redshift_tmp_dir = redshift_options["redshiftTmpDir"],
+    transformation_ctx = "datasource"
+)
 
-# Stop SparkSession
-spark.stop()
+# Write data to S3
+glueContext.write_dynamic_frame.from_options(
+    frame = datasource,
+    connection_type = "s3",
+    connection_options = {"path": s3_output_path},
+    format = "parquet",
+    format_options = {"compression": "SNAPPY"},
+    transformation_ctx = "datasink"
+)
+
+# Commit the job
+job.commit()
