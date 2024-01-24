4399fb05175bb9738d6a2f0a882d48c84055d33b
VenkatachalapathySubbaraj <87265462+VenkySubbaraj@users.noreply.github.com>
2024-01-24 17:03:15 +0530

    Update json_to_csv.py

--- json_to_csv/json_to_csv.py
+++ json_to_csv/json_to_csv.py
@@ -1,20 +1,13 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, explode_outer
 
-# Create a Spark session
 spark = SparkSession.builder.appName("rationalize_struct").getOrCreate()
 
 f = open('./Sample_data.json')
 data = json.load(f)
 print(data)
-
-# JSON data
 json_data = data
-
-# Create a DataFrame from JSON data
 df = spark.read.json(spark.sparkContext.parallelize(json_data, 1))
-
-# Extract the nested structures inside arrays
 df_batters = df.select(
     col("id"),
     col("type"),
@@ -28,8 +21,6 @@ df_toppings = df.select(
     col("name"),
     explode_outer("topping").alias("topping")
 )
-
-# Flatten the nested structures
 df_rationalized = df_batters.select(
     col("id"),
     col("type"),
@@ -47,14 +38,9 @@ df_rationalized = df_batters.select(
     ["id","type","name"],"outer"
 )
 
-# Show the result
 df_rationalized.show(truncate=False)
-
 csv_path = "./sample.csv"
 df_rationalized.write.csv(csv_path, header=True, mode="overwrite")
-
 csv_path = "./sample.parquet"
 df_rationalized.write.parquet(csv_path, mode="overwrite")
-
-# Stop the Spark session
 spark.stop()
